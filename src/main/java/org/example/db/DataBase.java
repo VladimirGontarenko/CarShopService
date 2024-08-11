e14fad4373dff0c3c26c23282dc835ee17a775be
VladimirGontarenko <mister.gontarenko@yandex.ru>
2024-08-11 23:43:59 +0400

    init commit

---
+++ src/main/java/org/example/db/DataBase.java
@@ -0,0 +1,49 @@
+package org.example.db;
+
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DataBase {
+    private static final String URL = "jdbc:postgresql://localhost:5433/car_shop";
+    private static final String USER = "admin";
+    private static final String PASSWORD = "admin123";
+    //private static final String URL = "jdbc:postgresql://localhost:5432/car_shop";
+//    private static final String USER = "user";
+//    private static final String PASSWORD = "password";
+    public Connection connection;
+    public Statement statement;
+
+    public DataBase() throws SQLException {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            System.out.println("SQL  " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        try {
+            Database database =
+                    DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            Liquibase liquibase =
+                    new Liquibase("db/changelog/changelog.xml", new ClassLoaderResourceAccessor(), database);
+            liquibase.update();
+            System.out.println("Migration is completed successfully");
+        } catch (LiquibaseException e) {
+            System.out.println("SQL Exception in migration " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
+
